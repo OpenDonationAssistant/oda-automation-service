f65915d95065d81addd035005b8a726a23d4f96e
stCarolas <stcarolas@gmail.com>
2026-09-02 09:15:30 +0300

    remove nonnull rule in triggerdto

--- src/main/java/io/github/opendonationassistant/automation/dto/AutomationTriggerDto.java
+++ src/main/java/io/github/opendonationassistant/automation/dto/AutomationTriggerDto.java
@@ -2,11 +2,10 @@ package io.github.opendonationassistant.automation.dto;
 
 import io.micronaut.serde.annotation.Serdeable;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.util.Map;
 
 @Serdeable
 public record AutomationTriggerDto(
   @NotBlank String id,
-  @NotNull Map<String, Object> value
+  Map<String, Object> value
 ) {}
