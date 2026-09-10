711a88c0ea2ec393118bda22fddfa665a907fffc
stCarolas <stcarolas@gmail.com>
2026-09-10 20:18:02 +0300

    Use wordblacklist when creating a new alert

--- src/main/java/io/github/opendonationassistant/automation/listener/messagehandlers/alert/PaymentEventHandler.java
+++ src/main/java/io/github/opendonationassistant/automation/listener/messagehandlers/alert/PaymentEventHandler.java
@@ -5,6 +5,7 @@ import io.github.opendonationassistant.alert.repository.AlertData;
 import io.github.opendonationassistant.alert.repository.AlertRepository;
 import io.github.opendonationassistant.events.AbstractMessageHandler;
 import io.github.opendonationassistant.events.payments.PaymentEvent;
+import io.github.opendonationassistant.wordblacklist.WordFilterRepository;
 import io.micronaut.serde.ObjectMapper;
 import jakarta.inject.Singleton;
 import java.io.IOException;
@@ -14,19 +15,28 @@ import java.time.Instant;
 public class PaymentEventHandler extends AbstractMessageHandler<PaymentEvent> {
 
   private final AlertRepository repository;
+  private final WordFilterRepository wordFilterRepository;
 
-  public PaymentEventHandler(ObjectMapper mapper, AlertRepository repository) {
+  public PaymentEventHandler(
+    ObjectMapper mapper,
+    AlertRepository repository,
+    WordFilterRepository wordFilterRepository
+  ) {
     super(mapper);
     this.repository = repository;
+    this.wordFilterRepository = wordFilterRepository;
   }
 
   @Override
   public void handle(PaymentEvent message) throws IOException {
+    var wordFilter = wordFilterRepository.getByRecipientId(
+      message.recipientId()
+    );
     var data = new AlertData(
       Generators.timeBasedEpochGenerator().generate().toString(),
       message.recipientId(),
-      message.cleanNickname(),
-      message.cleanMessage(),
+      wordFilter.filter(message.cleanNickname()),
+      wordFilter.filter(message.cleanMessage()),
       message.amount(),
       null,
       null,
